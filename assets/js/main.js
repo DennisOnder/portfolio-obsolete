f0e8bde8de5639088eff1b377a2a6bff07f4e3b7
Dennis Onder <35642244+DennisOnder@users.noreply.github.com>
2018-06-13 16:10:04 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
-var links = document.querySelectorAll('.a');
+/* var links = document.querySelectorAll('.a');
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', function () {
         var home = document.getElementById('home');
         home.style.overflow = "auto";
     });
 }
+*/
+
+//Removed
 
 function smoothScroll(e) {
     if (e == undefined || e == "") return;
